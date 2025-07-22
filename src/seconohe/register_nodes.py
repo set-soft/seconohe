ef718a4d796a421cb40474c2c2fcb7a34e833314
Salvador E. Tropea <salvador@inti.gob.ar>
2025-07-22 10:11:59 -0300

    [Added] Nodes register helper

---
+++ src/seconohe/register_nodes.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2025 Salvador E. Tropea
+# Copyright (c) 2025 Instituto Nacional de Tecnologïa Industrial
+# License: GPL-3.0
+# Project: SeCoNoHe
+# Helper to register all nodes from the provided modules
+import inspect
+import logging
+from . import settings
+
+logger = logging.getLogger(f"{settings.NODES_NAME}.register_nodes")
+
+
+def register_nodes(modules):
+    NODE_CLASS_MAPPINGS = {}
+    NODE_DISPLAY_NAME_MAPPINGS = {}
+
+    for m in modules:
+        for name, obj in inspect.getmembers(m):
+            # We skip nodes imported from the ComfyUI main nodes
+            if not inspect.isclass(obj) or not hasattr(obj, "INPUT_TYPES") or obj.__module__ == "nodes":
+                continue
+            assert hasattr(obj, "UNIQUE_NAME"), f"No name for {obj.__name__}"
+            NODE_CLASS_MAPPINGS[obj.UNIQUE_NAME] = obj
+            NODE_DISPLAY_NAME_MAPPINGS[obj.UNIQUE_NAME] = obj.DISPLAY_NAME
+
+    logger.info(f"Registering {len(NODE_CLASS_MAPPINGS)} node(s).")
+    logger.debug(f"{list(NODE_DISPLAY_NAME_MAPPINGS.values())}")
+
+    return NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
