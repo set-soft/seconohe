b48cbb7a601ab4f8cc4f8c4c5fd9a6fd1218562e
Salvador E. Tropea <salvador@inti.gob.ar>
2025-07-23 08:35:25 -0300

    [Register Nodes][Added] Suffix

--- src/seconohe/register_nodes.py
+++ src/seconohe/register_nodes.py
@@ -14,13 +14,16 @@ def register_nodes(logger: logging.Logger, modules: List[types.ModuleType]):
     NODE_DISPLAY_NAME_MAPPINGS = {}
 
     for m in modules:
+        suffix = " " + m.SUFFIX if hasattr(m, "SUFFIX") else ""
+        if suffix:
+            suffix = " " + suffix
         for name, obj in inspect.getmembers(m):
             # We skip nodes imported from the ComfyUI main nodes
             if not inspect.isclass(obj) or not hasattr(obj, "INPUT_TYPES") or obj.__module__ == "nodes":
                 continue
             assert hasattr(obj, "UNIQUE_NAME"), f"No name for {obj.__name__}"
             NODE_CLASS_MAPPINGS[obj.UNIQUE_NAME] = obj
-            NODE_DISPLAY_NAME_MAPPINGS[obj.UNIQUE_NAME] = obj.DISPLAY_NAME
+            NODE_DISPLAY_NAME_MAPPINGS[obj.UNIQUE_NAME] = obj.DISPLAY_NAME + suffix
 
     logger.info(f"Registering {len(NODE_CLASS_MAPPINGS)} node(s).")
     logger.debug(f"{list(NODE_DISPLAY_NAME_MAPPINGS.values())}")
